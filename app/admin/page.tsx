8933c1507f939b7a746b91a838bddd51d0566a88
Antonio Junior <jr.antoniojrr@GMAIL.COM>
2026-02-25 21:54:13 -0300

    Force dynamic admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
