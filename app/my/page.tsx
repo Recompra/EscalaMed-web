be5c8f2282c7486aaccb8ef981bc30171e06025c
Antonio Junior <jr.antoniojrr@GMAIL.COM>
2026-03-22 08:08:18 -0300

    reverter commit

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { createClient } from "@/lib/supabaseClient";
-const supabase = createClient();
+import { supabase } from "@/lib/supabaseClient";
 import { UFS, CITIES_BY_UF } from "@/data/cities";
 
 const SPECIALTIES = [
