bc1ab36cceea6d4e225acb9dc2c056b566bb0b3b
Antonio Junior <jr.antoniojrr@GMAIL.COM>
2026-03-22 07:47:04 -0300

    erro ao carregar medicos cadastrados

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -136,9 +136,10 @@ export default function MyPage() {
       return;
     }
 
-   const { data, error } = await supabase
+  const { data, error } = await supabase
   .from("doctors")
   .select("*")
+  .eq("tenant_id", user.id)  // 👈 adicionar essa linha
   .order("created_at", { ascending: false });
 
 console.log("DOCTORS TEST:", { error, data });
